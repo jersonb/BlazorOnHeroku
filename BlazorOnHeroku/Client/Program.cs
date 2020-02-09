3b9c29855ecf0faeb62747ccb8f4e432e7cc60f4
jersonb <jersonlbrito@hotmail.com>
2020-02-09 12:11:17 -0300

    change methds

--- BlazorOnHeroku/Client/Program.cs
+++ BlazorOnHeroku/Client/Program.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
 using System.Threading.Tasks;
-using System.Text;
-using Microsoft.AspNetCore.Blazor.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace BlazorOnHeroku.Client
 {
     public class Program
     {
+      
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
--- BlazorOnHeroku/Server/Controllers/WeatherForecastController.cs
+++ BlazorOnHeroku/Server/Controllers/WeatherForecastController.cs
@@ -1,10 +1,9 @@
 ﻿using BlazorOnHeroku.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace BlazorOnHeroku.Server.Controllers
 {
